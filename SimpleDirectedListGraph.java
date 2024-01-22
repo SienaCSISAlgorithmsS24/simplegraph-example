8b024e4c48b0119d89308752a8c136252e8ffef3
jteresco <terescoj@gmail.com>
2024-01-21 20:09:24 -0500

    Starter stubs of 2 classes.

---
+++ SimpleDirectedListGraph.java
@@ -0,0 +1,17 @@
+
+/**
+ * SimpleDirectedListGraph: a very simple directed graph stored with adjacency
+ * lists
+ *
+ * @author Jim Teresco
+ */
+public class SimpleDirectedListGraph
+{
+    /**
+     * Constructor
+     */
+    public SimpleDirectedListGraph(int numVerts)
+    {
+    }
+
+}
---
+++ SimpleDirectedMatrixGraph.java
@@ -0,0 +1,18 @@
+
+/**
+ * SimpleDirectedMatrixGraph: a very simple directed graph stored
+ * with an adjacency matrix
+ *
+ * @author Jim Teresco
+ */
+public class SimpleDirectedMatrixGraph
+{
+    
+    /**
+     * Constructor
+     */
+    public SimpleDirectedMatrixGraph(int numVerts)
+    {
+    }
+
+}
